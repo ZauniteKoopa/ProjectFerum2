51466e0ef603096a6c7904ee74afe7c0eecd7251
Anthony Hoang <anthonyhoang3074@gmail.com>
2020-07-15 15:31:49 -0500

    Tested movement() AbstractEnemy, moved player controller to allow control over 3 fighters, implemented very simple fighter switching, polish general UI

---
+++ Project_Ferum_V2/Assets/Scripts/EnemyAI/LinearMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Move horizontally left and right  (VECTOR MAY CHANGE)*/
+public class LinearMovement : AbstractEnemy
+{ 
+    //Movement variables
+    private const float MAX_DISTANCE = 10f;
+    private float curDist = 0f;
+    private bool moveRight = true;
+
+    //Boolean flag that keeps track of the last direction to ensure you only change once per frame
+    //  Used in the case where both movement() pivot same time as collision pivot
+    //  Update (movement()) is always checked before Collision2D
+    private bool lastMoveRight = true;
+
+
+    public override void movement() {
+        /* Set lastMoveRight to current moveRight */
+        lastMoveRight = moveRight;
+
+        /* Get movement speed */
+        float moveSpeed = getMoveSpeed();
+
+        /* Move unit */
+        Vector3 moveVect = Vector3.right * moveSpeed;
+        transform.position += (moveRight) ? moveVect : moveVect * -1;
+        curDist += moveSpeed;
+
+        /* Change direction */
+        if (curDist >= MAX_DISTANCE) {
+            moveRight = !moveRight;
+            curDist = 0f;
+        }
+
+    }
+
+    public override void attack() {
+
+    }
+
+    /* When colliding wall, reverse directions immediately */
+    void OnCollisionEnter2D(Collision2D collision) {
+        string colliderTag = collision.collider.tag;
+
+        /* Valid collision = Player && wall */
+        bool hit = colliderTag == GeneralConstants.WALL_TAG || colliderTag == GeneralConstants.PLAYER_TAG;
+
+        /* Checks if valid collision and that moveRight was only updated once */
+        if (hit && lastMoveRight == moveRight) {
+            moveRight = !moveRight;
+            curDist = 0f;
+        }
+    }
+}
--- Project_Ferum_V2/Assets/Scripts/Player/PlayerController.cs
+++ Project_Ferum_V2/Assets/Scripts/Player/PlayerController.cs
@@ -9,19 +9,33 @@ public class PlayerController : MonoBehaviour
     private int vDir = 0;
 
     /* Reference variable to entity associated with this controller (TO BE DELETED)*/
-    private EntityStatus status;
+    private const int MAX_NUM_FIGHTERS = 3;
+    [SerializeField]
+    private EntityStatus[] fighters = new EntityStatus[MAX_NUM_FIGHTERS];
+    private int mainIndex = 0;
+    private int numLiving = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
-        status = GetComponent<EntityStatus>();
+        /* Count how many ,living fighters at the start of the game */
+        for(int i = 0; i < fighters.Length; i++) {
+            if (i >= MAX_NUM_FIGHTERS)
+                throw new System.Exception("ERROR: Too many slots in fighters on this player controller");
+            
+            if (fighters[i] != null)
+                numLiving++;
+        }
+
+        if(numLiving == 0)
+            throw new System.Exception("ERROR: Player controller has no fighters initially");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         /* If cannot move, you don't move or attack */
-        if(status.canMove()) {
+        if(fighters[mainIndex].canMove()) {
             movement();
             attack();
         }
@@ -52,11 +66,18 @@ public class PlayerController : MonoBehaviour
             hDir = 0;
         }
 
-        /* Actually move the transform */
+        /* Make speed calculations */
         Vector3 moveDir = new Vector3(hDir, vDir, 0);
         moveDir.Normalize();
-        float curSpeed = status.getMovementSpeed();
-        transform.position += moveDir * curSpeed;
+        float curSpeed = fighters[mainIndex].getMovementSpeed();
+
+        /* Move the fighter first and then move the overall controller transform to fighter's position */
+        fighters[mainIndex].transform.position += moveDir * curSpeed;
+        transform.position += fighters[mainIndex].transform.localPosition;
+
+        /* Reset fighter's local position to avoid continous movement */
+        fighters[mainIndex].transform.localPosition = Vector3.zero;
+
 
         /* If player didn't move, go back to previous dir values */
         if(hDir == 0 && hDir == vDir) {
@@ -69,13 +90,54 @@ public class PlayerController : MonoBehaviour
 
     /* Helper method for attacking: allows player to attack */
     void attack() {
-        if (Input.GetKeyDown(ControlMap.ABILITY_1) && status.canUseMove(0)) {
-            StartCoroutine(status.executeMovePlayer(0, hDir, vDir));
-        } else if (Input.GetKeyDown(ControlMap.ABILITY_2) && status.canUseMove(1)) {
-            StartCoroutine(status.executeMovePlayer(1, hDir, vDir));
-        } else if (Input.GetKeyDown(ControlMap.ABILITY_3) && status.canUseMove(2)) {
-            StartCoroutine(status.executeMovePlayer(2, hDir, vDir));
+        /* Doing your main selected fighter's 3 abilities */
+        if (Input.GetKeyDown(ControlMap.ABILITY_1) && fighters[mainIndex].canUseMove(0)) {
+            StartCoroutine(fighters[mainIndex].executeMovePlayer(0, hDir, vDir));
+        } else if (Input.GetKeyDown(ControlMap.ABILITY_2) && fighters[mainIndex].canUseMove(1)) {
+            StartCoroutine(fighters[mainIndex].executeMovePlayer(1, hDir, vDir));
+        } else if (Input.GetKeyDown(ControlMap.ABILITY_3) && fighters[mainIndex].canUseMove(2)) {
+            StartCoroutine(fighters[mainIndex].executeMovePlayer(2, hDir, vDir));
         }
+
+        /* Swapping characters */
+        if(numLiving > 1) {
+            if(Input.GetKeyDown(ControlMap.SWAP_LEFT)) {
+                swapFighter(false);
+            }else if(Input.GetKeyDown(ControlMap.SWAP_RIGHT)) {
+                swapFighter(true);
+            }
+        }
+    }
+
+    /* Swap characters method: swapRight == true is adding, else subtracting
+        Pre: numiving > 1 
+        Post: previous fighter will deactivate and new fighter will activate */
+    void swapFighter(bool swapRight) {
+        Debug.Assert(numLiving > 1);
+
+        /* Get swap number depending on boolean flag */
+        int swapNumber = (swapRight) ? 1 : -1;
+
+        /* Find the new index representing the fighter you want to swap to */
+        int newIndex = mainIndex;
+
+        do
+        {
+            /* Move to the next fighter */
+            newIndex += swapNumber + MAX_NUM_FIGHTERS;
+            newIndex %= MAX_NUM_FIGHTERS;
+
+        } while (fighters[newIndex] == null);
+        
+        /* Assert that you got an entirely different fighter*/
+        Debug.Assert(newIndex != mainIndex);
+
+        /* Disable old fighter and enable new fighter */
+        fighters[mainIndex].gameObject.SetActive(false);
+        fighters[newIndex].gameObject.SetActive(true);
+
+        /* Update main Index */
+        mainIndex = newIndex;
     }
 
 }
